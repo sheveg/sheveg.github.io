1e060c688260e7601b74d0238070b6858ab528a0
Egor Shevdin <egor_sh@live.de>
2020-04-07 11:35:31 +0200

    asd

---
+++ README.md
@@ -0,0 +1,7 @@
+# Decrypto
+
+https://whoawhoa.github.io/decrypto/
+
+## Decrypto - How To Play
+
+[Youtube - How to Play](https://www.youtube.com/watch?v=2DBg7Z2-pQ4)
---
+++ decrypto.js
@@ -0,0 +1,136 @@
+var wordList;
+
+function loadWordList() {
+	var wordList;
+
+	$.ajax({
+            url : "wordlist.txt",
+            dataType: "text",
+            success : function (data) {
+            	wordList = data.split("\n");
+            },
+            async: false
+        });
+	return wordList;
+}
+
+function pickWords(numWords) {
+	return sampleWords(numWords);
+	//return _.sample(wordList, numWords);
+}
+
+function sampleWords(numWords) {
+	let si = Math.floor(wordList.length * Math.random());
+	return wordList.slice(si, si + numWords);
+}
+
+function generateCode() {
+	var code = _.sample([1,2,3,4], 3);
+	Cookies.set("code", code);
+	setCode(code);
+	$('#codeModal').modal('show');
+}
+
+function setCode(code) {
+	for(idx = 0; idx < 3; idx++) {
+		$('#code' + idx).text(code[idx]);
+	}
+	$('#revealCodeButton').show();
+}
+
+function setWords(words) {
+	for(idx = 0; idx < 4; idx++) {
+		$('#word' + idx).text(words[idx]);
+	}
+}
+
+function loadWords() {
+	return Cookies.getJSON("words");
+}
+
+function loadCode() {
+	var code = Cookies.getJSON("code");
+	if (code) {
+		setCode(code);
+	} else {
+		$('#revealCodeButton').hide();
+	}
+}
+
+function newGame() {
+	var words = pickWords(4);
+	Cookies.set("words", words);
+	Cookies.remove("code");
+	$('#revealCodeButton').hide();
+	return words;
+}
+
+function toggleFullScreen() {
+	if (screenfull.isFullscreen) {
+		screenfull.exit();
+	} else {
+		screenfull.request();
+	}
+	setFullScreenIcon();
+}
+
+function setFullScreenIcon() {
+    if (screenfull.isFullscreen) {
+		$('#enableFullScreen').hide();
+		$('#disableFullScreen').show();
+    } else {
+		$('#enableFullScreen').show();
+		$('#disableFullScreen').hide();
+    }
+}
+
+function startNewGame() {
+	setWords(newGame());
+}
+
+function disableScreenLock() {
+	var noSleep = new NoSleep();
+	noSleep.enable();
+}
+
+function initScreenfull() {
+	var noSleep = new NoSleep();
+
+	if (screenfull.enabled) {
+		screenfull.on('change', () => {
+			if (screenfull.isFullscreen) {
+				noSleep.enable();
+			} else {
+				noSleep.disable();
+			}
+		});
+        setInterval(setFullScreenIcon, 200);
+    } else {
+        $('#fullScreenButton').hide();
+        $('#disableScreenLockModal').modal('show');
+    }
+}
+
+function initialize() {
+	initScreenfull();
+
+	wordList = loadWordList();
+	loadCode();
+
+	var words = loadWords();
+
+	if (words) {
+		setWords(words);
+	} else {
+		/*
+		startNewGame();
+		$('#newGameModalCancelButton').hide();
+		$('#newGameModal').modal({
+			show: true,
+			keyboard: false,
+			backdrop: 'static'
+		});
+		*/
+		$('#newGameModal').modal('show');
+	}
+}
